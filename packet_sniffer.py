2f69057f772298cce02f8cc4249173becadf3fb3
d4rk-c0d3r <64041468+d4rk-c0d3r@users.noreply.github.com>
2020-04-21 10:05:30 -0500

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -5,7 +5,7 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 def get_url(packet):
-    return packet[http.HTTPRequest].Host + packet[httssp.HTTPRequest].Path
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
 
 def get_login_info(packet):
     if (packet.haslayer(scapy.Raw)):
